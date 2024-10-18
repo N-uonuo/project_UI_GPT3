ba5f4d57cbe4f2c3415fa71ccb991f28bb171789
N-uonuo <zalan2530@gmail.com>
2024-10-18 16:50:14 +0800

    feat：完成商标界面（添加了鼠标在上边时的样式变化）

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Brand.css';
+import {google, slack, atlassian, dropbox, shopify} from './imports';
 
 const Brand = () => {
     return (
-        <div>
-            
+        <div className="gpt3__brand section__padding ">
+            <div className="icon">
+                <a href="https://www.google.com" target="_blank" rel="noreferrer"><img src={google} alt="Google"/></a>
+            </div>
+            <div className="icon">
+                <img src={slack} alt="Slack"/>
+            </div>
+            <div className="icon">
+                <img src={atlassian} alt="Atlassian"/>
+            </div>
+            <div className="icon">
+                <img src={dropbox} alt="Dropbox"/>
+            </div>
+            <div className="icon">
+                <img src={shopify} alt="Shopify"/>
+            </div>
         </div>
     );
 };
